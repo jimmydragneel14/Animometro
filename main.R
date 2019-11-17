4792b111a7c915680f631ce210efe59a43ff4a17
Stefano Gerli <sgerli@igerli.com>
2019-11-17 08:49:30 -0600

    Order

--- main.R
+++ main.R
@@ -1,6 +1,9 @@
 getwd()
 # Deberia ser directorio del proyecto
 
+# ===================
+#   Setup datos
+# ===================
 data <- read.table("data.csv", header = TRUE, sep=",")
 data$date <- as.Date(data$date, format = "%m/%d/%Y %H:%M")
 data$class_week <- as.integer(strftime(data$date, format = "%V")) - 30
@@ -8,19 +11,34 @@ data$week_day <- as.integer(strftime(data$date, format = "%u"))
 data$day_hour <- as.integer(strftime(data$date, format = "%H"))
 
 
-#ggplot2
+# ===================
+#   Setup graficos
+# ===================
 library(ggplot2)
 
-#grafica
-
-ggplot(data = data, 
-       mapping =  aes(x = factor(week_day))) + 
-  geom_bar() + 
-  coord_flip()
-
-#fill con gear
-p <- ggplot(data = data, 
+# Por dia de la semana
+pwd <- ggplot(data = data, 
             mapping =  aes(x = factor(week_day), fill = factor(happy),  label = "count"))
+
+# Por semana del año
+pw <- ggplot(data = data, 
+              mapping =  aes(x = factor(class_week), fill = factor(happy),  label = "count"))
+
+# ===================
+#      Graficos
+# ===================
+
+# Felicidad por dia de la semana
+pwd +  geom_bar(position = "fill", stat = "count") 
+
+# Felicidad por semana del año
+pw +  geom_bar(position = "fill", stat = "count") 
+
+
+
+# ==================
+#     Ejemplos
+# ==================
 
 #stack
 p + geom_bar(position = "stack", stat = "count") 
