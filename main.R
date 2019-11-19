723885270decb9f0e12d1c46d5dc7699061d0c16
jimmydragneel14 <jimmymokz14@gmail.com>
2019-11-19 10:56:24 -0600

    Regresion lineal (varias grafica)
    Semana del semestre, dia de la semana, hora del dia, y fecha

--- main.R
+++ main.R
@@ -82,9 +82,48 @@ pwh + scale_y_continuous(labels = scales::percent) +
     subtitle = "Felicidad & Tristeza"
   )
 
+# Regresion lineal
+#====================
 
+ggplot(data, aes(x = data$week_day, y =data$happy)) + geom_point() + geom_smooth(method="lm") + 
+  ggtitle("Regresion lineal por dia de la semana") +
+  labs(x  =  'Dia de la semana', y  =  'Nivel de felicidad') 
 
-# ==================
+ggplot(data, aes(x =data$date_parsed, y = data$happy)) + geom_point() + geom_smooth(method="lm") +
+  ggtitle("Regresion lineal por fecha") +
+  labs(x  =  'Fecha', y  =  'Nivel de felicidad')
+
+ggplot(data, aes(x =data$class_week, y= data$happy)) + geom_point() + geom_smooth(method="lm") +
+  ggtitle("Regresion lineal por semanas del semestre") +
+  labs(x  =  'Semanas del semestre', y  =  'Nivel de felicidad')
+
+ggplot(data, aes(x =data$hour,y= data$happy)) + geom_point() + geom_smooth(method="lm") +
+  ggtitle("Regresion lineal por hora del dia") +
+  labs(x  =  'Hora del dia', y  =  'Nivel de felicidad')
+
+#Otras graficas de regresion lineal con datos "summary" y "predict"
+#====================================================================
+
+plot(data$date_parsed,data$happy,xlab = "Date",ylab = "Level of Happiness") 
+abline(lm(data$happy ~ data$date_parsed))
+summary(lm(data$happy ~ data$date_parsed))
+predict(lm(data$happy ~ data$date_parsed))
+
+plot(data$class_week, data$happy,xlab ="Class week",ylab = "Level of Happiness")
+abline(lm(data$happy ~ data$class_week))
+summary(lm(data$happy ~ data$class_week))
+predict(lm(data$happy ~ data$class_week))
+
+plot(data$week_day, data$happy,xlab = "Week Day" ,ylab = "Level of Happiness")
+abline(lm(data$happy ~ data$week_day))
+summary(lm(data$happy ~ data$week_day))
+predict(lm(data$happy ~ data$week_day))
+
+plot(data$hour, data$happy,xlab = "Hour", ylab = "Level of Happiness")
+abline(lm(data$happy ~ data$hour))
+summary(lm(data$happy ~ data$hour))
+predict(lm(data$happy ~ data$hour))
+
 #     Ejemplos
 # ==================
 
